c67a3ac17f9c1c16359305e2c5020e9353e609fc
VictorKevz <victor.kuwandira@student.oulu.fi>
2025-05-09 20:41:49 +0300

    remove unwanted code

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,6 @@ function App() {
   return (
     <ThemeProvider>
       <main className="w-full min-h-screen bg-[var(--bg)] flex flex-col items-center justify-center gap-4 relative">
-        <h1 className="text-2xl text-[var(--text)] font-bold">
-          Welcome to your project!
-        </h1>
         <Example />
         <ToggleButton />
       </main>
